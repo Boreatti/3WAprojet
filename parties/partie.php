a106eefa14e4ec03854a06915187a9b320df39d0
Morgan Boreatti <m.boreatti@gmail.com>
2017-12-14 16:01:13 +0100

    Add files via upload

---
+++ parties/partie.php
@@ -0,0 +1,25 @@
+<?php 
+//chargement du modele
+include('../script/init.php');
+
+
+//Verification de l'id de la partie chargée 
+$erreur='';
+ // on vérifie si on a bien une clef 'id' en GET
+		if(isset($_GET['id']) && is_numeric($_GET['id'])){
+		    // on vérifie s'assure que l'id ne va pas au dela de 6
+		    if(($_GET['id']) < 6){
+		        // on tente de charger les elements de partie
+				$part = getPartie('*');
+				$listeChpPartie = getChpPartie($_GET['id']); 
+		        
+		    }
+		    else{
+		        $erreur = "Oups ! Cette partie n'existe pas : partie " . $_GET['id'];
+		    }
+		}else{
+		    $erreur = 'Aucune partie séléctionnée :(';
+		}
+
+
+include('partieTemplate.phtml');
---
+++ script/config.exemple.php
@@ -0,0 +1,8 @@
+<?php
+// On declare un tableau de configuration vide 
+$config = [];
+$config['dbname'] = '*******';
+$config['dbhost'] = '*******';
+$config['dbuser'] = '*******';
+$config['dbpassword'] = '*******';
+$config['adminpassword'] = '$2y$10$M6mLkXrur4sbVBLRpUJZf.Ac5FcK./sINq0EsDj.g851bmTu2YH/y'; //chattons
